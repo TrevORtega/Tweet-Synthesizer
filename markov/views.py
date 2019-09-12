1d9a10d25f67f99c645c4bc4804bcc9da9a16078
Trevor Ortega <trevor.ortega.12@gmail.com>
2019-09-11 18:56:56 -0600

    Comments for views.py

--- markov/views.py
+++ markov/views.py
@@ -8,23 +8,26 @@ from markov.scraper2.markovey import *
 
 
 def index(request):
+    # Render the index page
     render (request, 'index.html')
     
+    # Ensure the form method from index is POST
     if request.method == 'POST':
-    
+        
+        # Get the form from index
         form = HandleForm(request.POST)
         if form.is_valid():
             #get the twitter handle from user
             handle = form.cleaned_data['handle']
             
-            
-            
+            # Get the new tweet from the main function in markovey
             tweet = main(handle)
             
+            # Redirect to the result page
             results_redirect(request)
             
             
-    
+            # Return the request and all of our information to the result page
             return render(request, 'results.html', {'newTweet' : tweet, 'user' : handle})
     else:
         form = HandleForm()
